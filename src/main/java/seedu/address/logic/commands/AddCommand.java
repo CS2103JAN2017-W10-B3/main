114c87dd61b48892bfed36b3225f01b76050d86c
yinatoh <yiina@live.com>
2017-03-14 21:07:10 +0800

    Change AddCommand to accept Optional parameters and depending on if there is a value in Optional, make the appropriate Task variables

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -22,8 +23,7 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a Task to the to-do list. "
-            + "Parameters: TITLE v/VENUE s/STARTTIME e/ENDTIME  [t/TAG]...\n"
-            + "Example: " + COMMAND_WORD
+            + "Parameters: TITLE v/VENUE s/STARTTIME e/ENDTIME  [t/TAG]...\n" + "Example: " + COMMAND_WORD
             + " CS2103 Tutorial v/COM1-B110 s/March 8,10.00am e/March 8, 11.00am t/lesson t/project";
 
     public static final String MESSAGE_SUCCESS = "New Task added: %1$s";
@@ -34,21 +34,38 @@ public class AddCommand extends Command {
     /**
      * Creates an AddCommand using raw values.
      *
-     * @throws IllegalValueException if any of the raw values are invalid
+     * @throws IllegalValueException
+     *             if any of the raw values are invalid
      */
-    public AddCommand(String title, String venue, String starttime, String endtime, Set<String> tags)
-            throws IllegalValueException {
+    public AddCommand(String title, Optional<String> venue, Optional<String> starttime, Optional<String> endtime,
+            Set<String> tags) throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
         }
-        this.toAdd = new Task(
-                new Title(title),
-                new Venue(venue),
-                new StartTime(starttime),
-                new EndTime(endtime),
-                new UniqueTagList(tagSet)
-        );
+
+        Title tempTitle = new Title(title);
+        Venue tempVenue = null;
+        StartTime tempStartTime = null;
+        EndTime tempEndTime = null;
+
+        if (venue.isPresent()) {
+            tempVenue = new Venue(venue.get());
+        } else {
+            tempVenue = new Venue("");
+        }
+        if (starttime.isPresent()) {
+            tempStartTime = new StartTime(starttime.get());
+        } else {
+            tempStartTime = new StartTime("");
+        }
+        if (endtime.isPresent()) {
+            tempEndTime = new EndTime(endtime.get());
+        } else {
+            tempEndTime = new EndTime("");
+        }
+
+        this.toAdd = new Task(tempTitle, tempVenue, tempStartTime, tempEndTime, new UniqueTagList(tagSet));
     }
 
     @Override
