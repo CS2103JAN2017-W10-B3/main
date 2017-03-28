b1e646e1c0f101caee66d91b291fb5f8ef81f809
Penghao Chen <xmchenph@gmail.com>
2017-03-28 18:15:23 +0800

    Create new class: CompleteTime

---
+++ src/main/java/todolist/model/task/CompleteTime.java
@@ -0,0 +1,23 @@
+package todolist.model.task;
+
+import java.time.LocalDateTime;
+
+public class CompleteTime implements Time {
+    
+    LocalDateTime completeTime;
+    
+    public CompleteTime(LocalDateTime time){
+        this.completeTime = time;
+    }
+    
+    @Override
+    public LocalDateTime getTimeValue() {
+        return this.completeTime;
+    }
+
+    @Override
+    public int compareTo(Time time) {
+        return this.completeTime.compareTo(time.getTimeValue());
+    }
+
+}
