422d7eb0dffa0db78beb3e69ec59897df9088754
Penghao Chen <xmchenph@gmail.com>
2017-03-28 14:33:01 +0800

    Update AddCommandTest: encapsulation for title

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -43,7 +43,7 @@ public class AddCommandTest extends ToDoListGuiTest {
         commandBox.runCommand(taskToAdd.getAddCommand());
 
         //confirm the new card contains the right data
-        TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getTitle().title);
+        TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getTitle().toString());
         assertMatching(taskToAdd, addedCard);
 
         //confirm the list now contains all previous Tasks plus the new Task
