bfad27cc618ccd4c1ec17ed0c060484e9d15e7e3
Penghao Chen <xmchenph@gmail.com>
2017-03-28 14:33:58 +0800

    Update BrowerPanel: encapsulation for title

--- src/main/java/todolist/ui/BrowserPanel.java
+++ src/main/java/todolist/ui/BrowserPanel.java
@@ -30,7 +30,7 @@ public class BrowserPanel extends UiPart<Region> {
     }
 
     public void loadTaskPage(ReadOnlyTask task) {
-        loadPage("https://www.google.com.sg/#safe=off&q=" + task.getTitle().title.replaceAll(" ", "+"));
+        loadPage("https://www.google.com.sg/#safe=off&q=" + task.getTitle().toString().replaceAll(" ", "+"));
     }
 
     public void loadPage(String url) {
