b739a2ef33212a8952d449775bd9ab533eae5fce
Penghao Chen <xmchenph@gmail.com>
2017-03-29 08:22:13 +0800

    Create class ListCommandParser

---
+++ src/main/java/todolist/logic/parser/ListCommandParser.java
@@ -0,0 +1,38 @@
+package todolist.logic.parser;
+
+import static todolist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static todolist.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+
+import todolist.logic.commands.Command;
+import todolist.logic.commands.FindCommand;
+import todolist.logic.commands.IncorrectCommand;
+import todolist.logic.commands.ListTaskUnderTagCommand;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class ListCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListTaskUnderTagCommand
+     * and returns an ListTaskUnderTagCommand object for execution.
+     */
+    public Command parseTag(String args) {
+        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListTaskUnderTagCommand.MESSAGE_USAGE));
+        }
+
+        // keywords delimited by whitespace
+        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
+        return new FindCommand(keywordSet);
+    }
+
+}
