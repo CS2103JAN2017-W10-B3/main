02baf12bf1508ad8db546a6a9800fee3d3f09dcd
Penghao Chen <xmchenph@gmail.com>
2017-03-29 08:24:59 +0800

    Update SelectCommandParser: include TaskIndex

--- src/main/java/todolist/logic/parser/SelectCommandParser.java
+++ src/main/java/todolist/logic/parser/SelectCommandParser.java
@@ -4,10 +4,10 @@ import static todolist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Optional;
 
-import javafx.util.Pair;
 import todolist.logic.commands.Command;
 import todolist.logic.commands.IncorrectCommand;
 import todolist.logic.commands.SelectCommand;
+import todolist.model.task.TaskIndex;
 
 /**
  * Parses input arguments and creates a new SelectCommand object
@@ -19,7 +19,7 @@ public class SelectCommandParser {
      * and returns an SelectCommand object for execution.
      */
     public Command parse(String args) {
-        Optional<Pair<Character, Integer>> index = ParserUtil.parseIndex(args);
+        Optional<TaskIndex> index = ParserUtil.parseIndex(args);
         if (!index.isPresent()) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
