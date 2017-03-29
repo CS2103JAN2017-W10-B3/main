07951bc8c240dbde312a22ece57b5fb5970c286a
Penghao Chen <xmchenph@gmail.com>
2017-03-29 08:21:55 +0800

    Update ListTaskUnderTagCommand: implement method

--- src/main/java/todolist/logic/commands/ListTaskUnderTagCommand.java
+++ src/main/java/todolist/logic/commands/ListTaskUnderTagCommand.java
@@ -1,5 +1,6 @@
 package todolist.logic.commands;
 
+import java.util.Set;
 
 /**
  * Lists all persons in the address book to the user.
@@ -7,11 +8,19 @@ package todolist.logic.commands;
 public class ListTaskUnderTagCommand extends Command {
 
     public static final String COMMAND_WORD = "list#";
-    public static final String MESSAGE_SUCCESS = "Tasks under tag \\p{Alnum}+ is listed!";
+    public static final String MESSAGE_SUCCESS = "Tasks under the tags are listed!";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " + tag name: list all tasks under the tag"
+            + "Example: " + COMMAND_WORD + " family friend school";
+    
+    private Set<String> keywordSet;
+    
+    public ListTaskUnderTagCommand(Set<String> keywordSet) {
+        this.keywordSet = keywordSet;
+    }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredListToShowAll();
+        model.updateFilteredTaskListToShowWithTag(keywordSet);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
