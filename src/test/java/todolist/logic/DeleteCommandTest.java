235b2040e4b0cbb23a72e808c2d8171f0d84267c
Penghao Chen <xmchenph@gmail.com>
2017-04-04 02:45:12 +0800

    Create class DeleteCommandTest: include all test cases for delete

---
+++ src/test/java/todolist/logic/DeleteCommandTest.java
@@ -0,0 +1,45 @@
+package todolist.logic;
+
+import static todolist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import todolist.logic.commands.DeleteCommand;
+import todolist.model.ToDoList;
+import todolist.model.task.Task;
+
+public class DeleteCommandTest extends LogicManagerTest {
+    
+    public static final int INDEX_FIRST_TASK = 1;
+    public static final String LINE_BREAK = "\n";
+    
+    @Test
+    public void execute_deleteInvalidArgsFormat_errorMessageShown() throws Exception {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE);
+        assertIncorrectIndexFormatBehaviorForCommand("delete", expectedMessage);
+    }
+
+    @Test
+    public void execute_deleteIndexNotFound_errorMessageShown() throws Exception {
+        assertIndexNotFoundBehaviorForCommand("delete");
+    }
+
+    @Test
+    public void execute_delete_removesCorrectTask() throws Exception {
+        TestDataHelper helper = new TestDataHelper();
+        List<Task> threeTasks = helper.generateTaskList(3);
+
+        ToDoList expectedAB = helper.generateToDoList(threeTasks);
+        expectedAB.removeTask(threeTasks.get(INDEX_FIRST_TASK));
+        helper.addToModel(model, threeTasks);
+
+        assertCommandSuccess("delete e2",
+                String.format(DeleteCommand.MESSAGE_DELETE_TASK_SUCCESS, 
+                        INDEX_FIRST_TASK, LINE_BREAK + threeTasks.get(INDEX_FIRST_TASK)),
+                expectedAB,
+                expectedAB.getFilteredEvents(), Task.EVENT_CHAR);
+    }
+    
+}
