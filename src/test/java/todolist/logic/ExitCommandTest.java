ed014bd6d58f55a3f271ce8e81f4b0ceec61aab0
Penghao Chen <xmchenph@gmail.com>
2017-04-04 03:14:13 +0800

    Create ExitCommandTest: migrate the test for exit

---
+++ src/test/java/todolist/logic/ExitCommandTest.java
@@ -0,0 +1,20 @@
+package todolist.logic;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+import todolist.logic.commands.ExitCommand;
+import todolist.model.ToDoList;
+import todolist.model.task.Task;
+
+//@@author A0122017Y
+public class ExitCommandTest extends LogicManagerTest {
+    
+    @Test
+    public void execute_exit() {
+        assertCommandSuccess("exit", ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT,
+                new ToDoList(), Collections.emptyList(), Task.ALL_CHAR);
+    }
+
+}
