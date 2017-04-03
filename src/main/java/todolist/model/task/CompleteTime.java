ec359c1558c4582934dedfc1f9e1c6e351f51f04
Penghao Chen <xmchenph@gmail.com>
2017-04-04 01:46:59 +0800

    Update CompleteTime: add comments

--- src/main/java/todolist/model/task/CompleteTime.java
+++ src/main/java/todolist/model/task/CompleteTime.java
@@ -2,14 +2,22 @@ package todolist.model.task;
 
 import java.time.LocalDateTime;
 
+//@@author A0122017Y
 public class CompleteTime implements Time {
 
     LocalDateTime completeTime;
-
+    
+    /**
+     * CompleteTime will be initiated with the current time at completing the task,
+     * since the complete time is only for sorting purpose, less API is created.
+     */
     public CompleteTime(LocalDateTime time) {
         this.completeTime = time;
     }
-
+    
+    /**
+     * Obtain the LocalDateTime value of the complete time
+     */
     @Override
     public LocalDateTime getTimeValue() {
         return this.completeTime;
