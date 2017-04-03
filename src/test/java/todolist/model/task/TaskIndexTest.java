3e9f2b6e245a2e28dd87c4c70fe25b1cbd8d1ff7
Penghao Chen <xmchenph@gmail.com>
2017-04-04 02:06:25 +0800

    Create class: TaskIndexTest

---
+++ src/test/java/todolist/model/task/TaskIndexTest.java
@@ -0,0 +1,23 @@
+package todolist.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TaskIndexTest {
+    
+    @Test
+    public void isValidTaskIndex() {
+        //invalid task index
+        assertFalse(TaskIndex.isValidTaskIndex('a', 4));  //task character not valid
+        assertFalse(TaskIndex.isValidTaskIndex('d', -3)); //task number not valid
+        
+        //valid task index
+        assertTrue(TaskIndex.isValidTaskIndex('d', 3));  //deadline task index
+        assertTrue(TaskIndex.isValidTaskIndex('e', 3));  //event task index
+        assertTrue(TaskIndex.isValidTaskIndex('f', 3));  //floating task index
+        assertTrue(TaskIndex.isValidTaskIndex('e', 1999999));  //task index with very big number
+    }
+    
+}
