bb9a4550aee525c06395f8750dbea643704ba456
Penghao Chen <xmchenph@gmail.com>
2017-04-08 21:35:25 +0800

    Update ListCommandParser: implement methods

--- src/main/java/todolist/logic/parser/ListCommandParser.java
+++ src/main/java/todolist/logic/parser/ListCommandParser.java
@@ -5,12 +5,8 @@ import static todolist.logic.parser.CliSyntax.PREFIX_BEGINNINGTIME;
 import static todolist.logic.parser.CliSyntax.PREFIX_DEADLINETIME;
 import static todolist.logic.parser.CliSyntax.PREFIX_ENDTIME;
 import static todolist.logic.parser.CliSyntax.PREFIX_STARTTIME;
-import static todolist.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 import java.util.regex.Matcher;
 
 import todolist.commons.exceptions.IllegalValueException;
@@ -34,19 +30,17 @@ public class ListCommandParser {
         if (!matcher.matches()) {
             return new ListCommand();
         }
-
-        // keywords delimited by whitespace
-        final String[] keywords = matcher.group("keywords").split("\\s+");
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         
         ArgumentTokenizer argsTokenizer =
                 new ArgumentTokenizer(PREFIX_STARTTIME, PREFIX_BEGINNINGTIME, PREFIX_ENDTIME,
-                        PREFIX_DEADLINETIME, PREFIX_TAG);
+                        PREFIX_DEADLINETIME);
+        argsTokenizer.tokenize(args);
         
         Optional<String> startTimeArg = argsTokenizer.getValue(PREFIX_STARTTIME);
         Optional<String> endTimeArg = argsTokenizer.getValue(PREFIX_ENDTIME);    
         Optional<String> beginningTimeArg = argsTokenizer.getValue(PREFIX_BEGINNINGTIME);
         Optional<String> deadlineArg = argsTokenizer.getValue(PREFIX_DEADLINETIME);
+        
         try {
             TimeUtil.checkTimeDuplicated(startTimeArg,beginningTimeArg, endTimeArg,deadlineArg);
         } catch (IllegalValueException e) {
