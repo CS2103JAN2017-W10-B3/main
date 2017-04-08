1e40bff6c31e0d1cb448771f4af5a7db84422d8c
Penghao Chen <xmchenph@gmail.com>
2017-04-08 09:48:15 +0800

    Update JokeCommand: changes in command message formats

--- src/main/java/todolist/logic/commands/JokeCommand.java
+++ src/main/java/todolist/logic/commands/JokeCommand.java
@@ -2,20 +2,26 @@ package todolist.logic.commands;
 
 import todolist.commons.util.JokesUtil;
 import todolist.logic.commands.exceptions.CommandException;
-
+//@@author A0122017Y
 public class JokeCommand extends Command {
     
     public static final String COMMAND_WORD = "joke";
     
-    private final String[] JOKES = JokesUtil.JOKES;
+    private static final String[] JOKES = JokesUtil.JOKES;
+
+    private static final String MESSAGE_SUCCESS = "You must be laughing :D";
     
     private int randomGenerator() {
         return (int) Math.floor(Math.random()*JOKES.length);
     }
-    @Override
-    public CommandResult execute() throws CommandException {
-        return new CommandResult(JOKES[randomGenerator()]);
+    
+    private String jokeGenerator() {
+        return JOKES[randomGenerator()] + "\n" + MESSAGE_SUCCESS;
     }
     
+    @Override
+    public CommandResult execute() throws CommandException {
+        return new CommandResult(jokeGenerator());
+    }
     
 }
