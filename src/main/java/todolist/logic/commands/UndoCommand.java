124c94cb6de266c48453666d70f83b32d073869f
Penghao Chen <xmchenph@gmail.com>
2017-04-09 11:21:05 +0800

    Update UndoCommand: update message

--- src/main/java/todolist/logic/commands/UndoCommand.java
+++ src/main/java/todolist/logic/commands/UndoCommand.java
@@ -3,6 +3,7 @@ package todolist.logic.commands;
 import java.util.logging.Logger;
 
 import todolist.commons.core.LogsCenter;
+import todolist.logic.commands.exceptions.CommandException;
 
 //@@author A0143648Y
 /**
@@ -18,9 +19,10 @@ public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Only the last three commands can be recovered. \n"
             + "Example: " + COMMAND_WORD + "\n";
+    public static final String MESSAGE_UNDO_SUCCESS = "The following command has been undone: ";
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws CommandException {
         try {
             logger.info("-----------[SYSTEM UNDO COMMAND]");
             model.resetData(UndoableCommand.previousToDoLists.get(UndoableCommand.previousToDoLists.size() - 1));
@@ -28,9 +30,11 @@ public class UndoCommand extends Command {
             String feedbackToUser = new String(UndoableCommand.previousCommandResults
                     .get(UndoableCommand.previousCommandResults.size() - 1).feedbackToUser);
             UndoableCommand.previousCommandResults.remove(UndoableCommand.previousCommandResults.size() - 1);
-            return new CommandResult("The following command has been undone:" + feedbackToUser);
+            return new CommandResult(MESSAGE_UNDO_SUCCESS + feedbackToUser);
         } catch (ArrayIndexOutOfBoundsException e) {
-            return new CommandResult(MESSAGE_UNDO_FAILURE);
+            throw new CommandException(MESSAGE_UNDO_FAILURE);
+        } catch (NullPointerException e) {
+            throw new CommandException(MESSAGE_UNDO_FAILURE);
         }
 
     }
