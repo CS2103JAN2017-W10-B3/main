cc2ad7104935c0a36ccab54d0d17959b038a01a7
Penghao Chen <xmchenph@gmail.com>
2017-04-09 21:21:03 +0800

    ParserUtilTest: Add new test cases

--- src/test/java/todolist/logic/parser/ParserUtilTest.java
+++ src/test/java/todolist/logic/parser/ParserUtilTest.java
@@ -14,10 +14,15 @@ public class ParserUtilTest {
         assertTrue(ParserUtil.isMultipleValidIndex("f12-13"));
         assertTrue(ParserUtil.isMultipleValidIndex("c5-33"));
         assertTrue(ParserUtil.isMultipleValidIndex("1-3"));
+        assertTrue(ParserUtil.isMultipleValidIndex("6-3"));
+        assertTrue(ParserUtil.isMultipleValidIndex("e99-3"));
+        assertTrue(ParserUtil.isMultipleValidIndex("f101-3"));
+        assertTrue(ParserUtil.isMultipleValidIndex("d4-3"));
 
         assertFalse(ParserUtil.isMultipleValidIndex("d1"));
         assertFalse(ParserUtil.isMultipleValidIndex("b1-3"));
         assertFalse(ParserUtil.isMultipleValidIndex("1"));
+        assertFalse(ParserUtil.isMultipleValidIndex("0"));
         assertFalse(ParserUtil.isMultipleValidIndex("d-1"));
         assertFalse(ParserUtil.isMultipleValidIndex("not number"));
 
