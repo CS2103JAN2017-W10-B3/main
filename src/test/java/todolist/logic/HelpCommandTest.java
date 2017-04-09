d55b247e12c41372152f2294bac86df0cc9283cc
Penghao Chen <xmchenph@gmail.com>
2017-04-09 11:35:49 +0800

    Resolve conflict

--- src/test/java/todolist/logic/HelpCommandTest.java
+++ src/test/java/todolist/logic/HelpCommandTest.java
@@ -15,11 +15,7 @@ public class HelpCommandTest extends LogicManagerTest {
 
     @Test
     public void executeHelp() {
-<<<<<<< HEAD
         assertCommandSuccess("help", HelpCommand.getHelpMessage(),
-=======
-        assertCommandSuccess("help", HelpCommand.showingHelpMessage,
->>>>>>> b751bfc52b5fdaf8a8c35232fb674e6b0b0d67ca
                 new ToDoList(), Collections.emptyList(), Task.ALL_CHAR);
         assertTrue(helpShown);
     }
