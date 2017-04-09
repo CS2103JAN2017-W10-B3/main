4eab680438ef6cf8305ec8dfd9c2ccfb8271974e
wangchi <wangchi92@gmail.com>
2017-04-10 07:00:49 +0800

    no message

--- src/main/java/todolist/ui/UiManager.java
+++ src/main/java/todolist/ui/UiManager.java
@@ -181,7 +181,6 @@ public class UiManager extends ComponentManager implements Ui {
     public void stop() {
         prefs.updateLastUsedGuiSetting(mainWindow.getCurrentGuiSetting());
         mainWindow.hide();
-        // mainWindow.releaseResources();
     }
 
     private void showFileOperationAlertAndWait(String description, String details, Throwable cause) {
@@ -216,8 +215,6 @@ public class UiManager extends ComponentManager implements Ui {
     private void showFatalErrorDialogAndShutdown(String title, Throwable e) {
         logger.severe(title + " " + e.getMessage() + StringUtil.getDetails(e));
         showAlertDialogAndWait(Alert.AlertType.ERROR, title, e.getMessage(), e.toString());
-        Platform.exit();
-        System.exit(1);
     }
 
     // ==================== Event Handling Code
